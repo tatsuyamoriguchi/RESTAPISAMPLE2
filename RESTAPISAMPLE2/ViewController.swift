8c716654c0d6b6ebc47857cb3ce1071fefa33dfa
Brian Moriguchi <brian@beckos.com>
2020-07-16 19:06:28 -0700

    Initial Commit

---
+++ RESTAPISAMPLE2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTAPISAMPLE2
+//
+//  Created by Tatsuya Moriguchi on 7/16/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
